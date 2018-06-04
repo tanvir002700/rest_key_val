652993c9d1810d7c47acc2b15d5fbb0bc2feff3e
tanvir <tanvir002700@gmail.com>
2018-06-04 14:15:37 +0600

    customize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
-  resources :stores
+  get '/values', to: 'stores#index'
+  post '/values', to: 'stores#create'
+  patch '/values', to: 'stores#update'
 end
