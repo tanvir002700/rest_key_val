04816edac910122f40b11c379f110b39a8b20784
tanvir <tanvir002700@gmail.com>
2018-06-04 13:17:36 +0600

    add post method

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,3 +1,23 @@
 class StoresController < ApplicationController
+  include ParamBuilder
 
+  def index
+    stores = Store.where("validity >= ?", Time.now)
+    render json: stores
+  end
+
+  def create
+    formatted_params = build_store_params(store_params)
+    Store.create(formatted_params)
+    render json: params
+  end
+
+  def update
+    formatted_params = build_store_params(store_params)
+
+  end
+
+  def store_params
+    params.require(:store)
+  end
 end
